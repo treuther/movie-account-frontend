4e27e52f26a09b573e723bd3f8a915f3abd3fbfb
treuther <tracyreuther@gmail.com>
2020-03-02 19:12:55 -0600

    trying to console.log movies

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
+
+  componentDidMount() {
+    return fetch('http://localhost:3000/api/v1/genres')
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }
+
   render() {
     return (
       <div className="App">

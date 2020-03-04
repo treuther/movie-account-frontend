4b765d081e226b5943743229834a9287e038f2bd
treuther <tracyreuther@gmail.com>
2020-03-04 13:15:28 -0600

    creationg of folders, genreReducer and fetchGenres action

---
+++ src/actions/fetchGenres.js
@@ -0,0 +1,5 @@
+//the action creates creates an action object, the action object gets dispatched to the reducer
+
+export function fetchGenres() {
+
+}
--- src/reducers/genreReducer.js
+++ src/reducers/genreReducer.js
@@ -1,5 +1,5 @@
 //reducer is automaticaly passed the previous state as its first argument
 
-export default function genreReducer(state = {genres = []}, action) {
+export default function genreReducer(state = {genres: []}, action) {
     return state
 }

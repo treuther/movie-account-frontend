3fb943375d7b36c08810fe9b6a03b1295bf7e5ec
treuther <tracyreuther@gmail.com>
2020-03-03 19:19:24 -0600

    successful console.log of movies

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
 class App extends React.Component {
 
   componentDidMount() {
-    return fetch('http://localhost:3000/api/v1/genres')
+    fetch('http://localhost:3000/api/v1/genres')
     .then(response => response.json())
     .then(data => console.log(data))
   }
+
+  // componentDidMount() {
+  //   console.log('are you there')
+  // }
 
   render() {
     return (
--- src/index.js
+++ src/index.js
@@ -5,3 +5,9 @@ import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+// app.use(express.static(__dirname)); //here is important thing - no static directory, because all static :)
+
+// app.get("/*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "index.html"));
+// });
+

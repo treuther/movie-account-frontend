617199ba0a0ca19918fb0002b1222cb1a1b3aa4f
treuther <tracyreuther@gmail.com>
2020-03-06 03:54:45 -0600

    setting up genre input form

--- src/components/GenreInput.js
+++ src/components/GenreInput.js
@@ -4,10 +4,25 @@ import React from 'react';
 
 class GenreInput extends React.Component {
 
+    state = {
+        name: ''
+    }
+
+    // abstracting the name KEY from input. Useful when there is more than one value to pass in. Brackets because it's reading the entered value and sets it as key. The rules of JS object assinging.
+    handleOnChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
     render() {
         return (
             <div>
-                Genre Input
+                <form>
+                    <label>Enter Genre Name:</label><br/>
+                    <input type="text" placeholder="Name" value={this.state.name} name="name" onChange={this.handleOnChange}/> <br/>
+                    <input type="submit" />
+                </form>
             </div>
         )
     }

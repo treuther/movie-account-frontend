fb0641999e4866713c9e7830012f11ca130d284d
treuther <tracyreuther@gmail.com>
2020-03-10 04:09:19 -0500

    setting up movieeinput

--- src/components/MovieInput.js
+++ src/components/MovieInput.js
@@ -6,18 +6,30 @@ import {connect} from 'react-redux';
 
 class MovieInput extends React.Component {
 
+    state = {
+        title: '',
+        rating: '',
+        description: ''
+    }
+
+    handleOnChange = () => {
+        //
+    }
+
     render() {
         return(
             <div>
                 <form>
                     <label>Enter title:</label><br />
-                    <input /><br/>
+                    <input type="text" name="title" value={this.state.title} onChange={this.handleOnChange}/><br/>
                     <label>Choose rating:</label>
-                    <select>
+                    <select name="rating" value={this.state.rating} onChange={this.handleOnChange}>
                         <option>R</option>
                         <option>PG-13</option>
                         <option>PG</option>
-                    </select>
+                    </select><br />
+                    <label>Enter description:</label><br />
+                    <input type="text" name="description" value={this.state.description} onChange={this.handleOnChange}/><br/>
                 </form>
             </div>
         )

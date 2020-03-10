58bc9b244f497d51cff7053ced9fc8028621e04d
treuther <tracyreuther@gmail.com>
2020-03-10 12:56:56 -0500

    fixed error. nMovieInput is now saving movies

--- src/reducers/genreReducer.js
+++ src/reducers/genreReducer.js
@@ -1,13 +1,21 @@
 //reducer is automaticaly passed the previous state as its first argument
 
 export default function genreReducer(state = {genres: []}, action) {
+    
     switch (action.type) {
         case 'FETCH_GENRES':
             return {genres: action.payload}
         case 'ADD_GENRE':
             return {...state, genres: [...state.genres, action.payload]} //returns an array of all previous genres, plus the new genre with action.payload.
         case 'ADD_MOVIE':
-            return{}
+            let genres = state.genres.map(genre => {
+                if (genre.id === action.payload.id) {
+                    return action.payload
+                } else {
+                    return genre
+                }
+            })
+            return {...state, genres: genres}
         default:
             return state
     }  

2f99d54c2b9d7ff5797e4aa5179c9095886934f7
treuther <tracyreuther@gmail.com>
2020-03-31 15:12:59 -0500

    review 2 edits

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -6,42 +6,64 @@ import {connect} from 'react-redux';
 import {deleteMovie} from '../actions/deleteMovie';
 import {Container, ListGroup, Item, Button} from 'react-bootstrap';
 
-const Movies = (props) => {
+class Movies extends React.Component {
+
+// const Movies = (props) => {
 
     // console.log(props)
 
-//defined with const keywords because this is a functional component.
-const handleDelete = (movie) => {
-    props.deleteMovie(movie.id, movie.genre_id) //because this is a functional componenet, it comes in a props, not this.props
+state = {
+    movies: []
 }
-let currentPath = window.location.pathname;
 
-    return (
-        <Container>
-            <h2>Recorded Movies</h2>
-            <ListGroup>
-                {props.movies && props.movies.map(movie => 
-                    <ListGroup.Item key={movie.id}><h3>{movie.title}</h3> <p>Rating: {movie.rating}</p><p>{movie.description}</p>
-                    <Button className="btn" variant="dark" onClick={() => handleDelete(movie)}>Delete</Button>
-                    </ListGroup.Item>
-                )}
-            </ListGroup>
-        </Container>
-        
-        // <Container>
-        //     <h2>Recorded Movies</h2>
-        //     <ListGroup>
-        //         <p>Click on a movie to see details.</p>
-        //         {props.movies && props.movies.map(movie =>
-        //             <ListGroup.Item key={movie.id}>
-        //                 {currentPath.includes('/movies') ? 
-        //                 <Link to={`/genres/${movie.genre_id}/movies/${movie.id}`}>{movie.title}</Link> : 
-        //                 <Link to={`/movies/${movie.id}`}>{movie.title}</Link>}
-        //                 {/* <Link to={`/movies/${movie.id}`}>{movie.title}</Link> */}
-        //             </ListGroup.Item>)}
-        //     </ListGroup>
-        // </Container>
-    )
+//defined with const keywords because this is a functional component.
+handleDelete = (movie) => {
+    this.props.deleteMovie(movie.id, movie.genre_id) //because this is a functional componenet, it comes in a props, not this.props
+}
+
+handleSort = () => {
+   console.log("hello")
+    let sortedList = [...this.props.movies].sort((a, b) => {
+        if (a.title < b.title) {
+            return -1;
+        } 
+        if (a.title > b.title) {
+            return 1;
+        }
+        return 0;
+    })
+    this.setState({
+        movies: sortedList
+    })
+}
+
+    render() {
+
+        let currentPath = window.location.pathname;
+        return (
+            <Container>
+                <h2>Recorded Movies</h2>
+                <Button className="btn" variant="dark" onClick={() => this.handleSort()}>Sort title</Button>
+                <ListGroup>
+                    {this.props.movies && this.props.movies.map(movie => 
+                        <ListGroup.Item key={movie.id}><h3>{movie.title}</h3> <p>Rating: {movie.rating}</p><p>{movie.description}</p>
+                        <Button className="btn" variant="dark" onClick={() => this.handleDelete(movie)}>Delete</Button>
+                        
+                        </ListGroup.Item>
+                    )}
+                </ListGroup>
+            </Container>
+        )
+    }
 }
 
 export default connect(null, {deleteMovie})(Movies);
+
+
+//# sort movies alphabetically by title
+// 1a. Change to class component
+// 1. Create a button
+// 2. Create a function to run for button click
+// 3. Access the array of movies (props.movies)
+// 4. Call sort() on array of movies (props.movies.sort())
+// 5. Display sorted array (state)
